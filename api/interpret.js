eb637aa00a7fc4aadad90bae2bd8ad11eaf7d5a9
Lily <lily@LilydeMacBook-Air.local>
2025-03-09 20:51:37 -0700

    vercel

--- api/interpret.js
+++ api/interpret.js
@@ -10,6 +10,7 @@ module.exports = async function handler(req, res) {
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS, GET");
     res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    res.setHeader("Connection", "keep-alive");
 
     // Handle CORS preflight
     if (req.method === "OPTIONS") {
